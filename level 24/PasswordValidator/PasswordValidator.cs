d68c84cb175b8f86561c6bed945863335a1ee13a
Brett Smith <brett03112@gmail.com>
2024-05-22 12:07:03 -0400

    Update work with level 24 on 5/22/2024

---
+++ level 24/PasswordValidator/PasswordValidator.cs
@@ -0,0 +1,35 @@
+﻿namespace Level24;
+
+public class PasswordValidator
+{
+    public bool Validate(string password)
+    {
+        int lower_letters = 0;
+        int upper_letters = 0;
+        int numbers = 0;
+        foreach (char c in password)
+        {
+            if (char.IsLower(c)) lower_letters++;
+            if (char.IsUpper(c)) upper_letters++;
+            if (char.IsDigit(c)) numbers++;
+            if(c == 'T' || c == '&')
+            {
+                WriteLine("Password cannot contain 'T' or '&'.");
+                return false;
+            }
+        }
+         if (lower_letters > 0 && upper_letters > 0 && numbers > 0)
+            {
+                WriteLine("Password is valid.");
+                return true;
+            }
+        else
+        {
+            WriteLine("Password is invalid.");
+            return false;
+        }
+        
+    }
+}
+
+
---
+++ level 24/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ *  Write a program that tests the classes made in level 24
+ *
+ *
+*/
+
+// Point class
+
+
+static void Main(string[] args)
+{
+    Point point = new Point();
+    point.X = 5;
+    point.Y = 10;
+    WriteLine($"X: {point.X}, Y: {point.Y}");
+
+
+}
