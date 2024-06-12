71af5dad8e4097e4ffe4a8bca9aa899b9e17f65d
Brett Smith <brett03112@gmail.com>
2024-06-12 14:56:23 -0400

    completed

---
+++ level 29/Notes/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+• Records are a compact alternative notation for defining a data-centric class or struct: public record Point(float X, float Y);
+
+• The compiler automatically generates a constructor, properties, ToString, equality with value semantics, and deconstruction.
+
+• You can add additional members or provide a definition for most compiler-synthesized members.
+
+• Records are turned into classes by default or into a struct (public record struct Point(...)).
+
+• Records can be used in a with expression: Point modified = p with { X = -2 };
+
+• Records can be used as an alternative to tuples due to the ToString override and equality with value semantics and deconstruction.
+*/
+
+using System.Drawing;
+
+Point a = new Point(2,3);
+Point b = new Point(2,3);
+WriteLine(a);
+WriteLine(b);
+WriteLine(a == b);
+
+Point p = new Point(-2, 5);
+(float x, float y) = p;
+WriteLine(p);
+
+Point p1 = new Point(-2, 5);
+Point p2 = p1 with { X = 0 }; // using the 'with' statement
+WriteLine(p2);
+
+Point p3 = p1 with {X = 0, Y = 0};
+WriteLine(p3);
+
+// records
+public record Point(float X, float Y) // That's all there is to it.
+{
+    public override string ToString() => $"({X}, {Y})";// Override ToString from records
+}
+
+public record ColoredPoint (Color color, float X, float Y) : Point(X, Y); // inheritance using a record
+
+
+
+
+public record Rectangle(float Width, float Height)
+{
+    public float Area => Width * Height; // computed property
+}
+
---
+++ level 29/WarPreparations/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+• Swords can be made out of any of the following materials: wood, bronze, iron, steel, and the rare binarium. Create an enumeration to represent the material type.
+
+• Gemstones can be attached to a sword, which gives them strange powers through Cygnus and Lyra’s 
+    touch. Gemstone types include emerald, amber, sapphire, diamond, and the rare bitstone. Or no
+    gemstone at all. Create an enumeration to represent a gemstone type.
+
+• Create a Sword record with a material, gemstone, length, and crossguard width.
+
+• In your main program, create a basic Sword instance made out of iron and with no gemstone. Then create two variations on the basic sword using with expressions.
+
+• Display all three sword instances with code like Console.WriteLine(original);.
+*/
+
+Sword basic = new Sword(SwordMaterial.Iron, Gemstone.None, 5, 1);
+Sword better = basic with { Gemstone = Gemstone.Emerald };
+Sword best = better with { Gemstone = Gemstone.Bitstone, Material = SwordMaterial.Steel, Length = 39, CrossguardWidth = 6 };
+
+WriteLine(basic);
+WriteLine(better);
+WriteLine(best);
+public record Sword(SwordMaterial Material, Gemstone Gemstone, float Length, float CrossguardWidth);
+
+public enum SwordMaterial
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium
+}
+
+public enum Gemstone
+{
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone,
+    None
+}
