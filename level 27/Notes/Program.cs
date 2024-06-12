4be60a761297427ba3ef73b5b992448c2a8511bc
Brett Smith <brett03112@gmail.com>
2024-06-12 13:59:52 -0400

    completed

--- level 27/Notes/Program.cs
+++ level 27/Notes/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Notes on Interfaces
+
+    https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8#interfaces
+    --Use the interface keyworrd instead of class
+    --members are public and abstract
+    --no need to implement the interface
+    --when used in a class, the class must implement the interface(define the methods within the interface)
+    --classes can implement multiple interfaces
+
+    https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8#interface-methods
+    --they form a contract or specification of the methods that a class must implement
+
+    --default implementation of interface methods can be used to simplify code where an interface implements multiple classes
+    --
+*/
+
+/*
+public interface ILevelBuilder
+{
+    Level BuildLevel(int levelNumber);
+    int Count {get; }
+
+    Level[] BuildAllLevels()
+    {
+        Level[] levels = new Level[Count];
+
+        for (int Index = 1; Index <= Count; Index++)
+            levels[Index - 1] = BuildLevel(Index);
+        
+        return levels;
+    }
+}
+
+public class FixedLevelBuilder : ILevelBuilder
+{
+    public Level BuildLevel(int levelNumber)
+    {
+        Level level = new Level(10, 10, TerrainType.Forests);
+
+        level.SetTerrainAt(2, 4, TerrainType.Mountains);
+        level.SetTerrainAt(2, 5, TerrainType.Mountains);
+        level.SetTerrainAt(6, 1, TerrainType.Desert);
+
+        return level;
+
+    }
+}
+
+public enum TerrainType { Desert, Forests, Mountains, Pastures, Fields, Hills }
+
+*/
+
+WriteLine("hello");
---
+++ level 27/RoboticInterface/Program.cs
@@ -0,0 +1,75 @@
+﻿Robot robot = new Robot();
+
+for (int index = 0; index < robot.Commands.Length; index++)
+{
+    WriteLine("Enter the 3 commands for the robot:  ");
+    string? input = ReadLine();
+    robot.Commands[index] = input switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "east" => new EastCommand(),
+        "west" => new WestCommand(),
+        _ => throw new NotImplementedException(),
+    };
+    
+}
+
+WriteLine();
+
+robot.Run();
+
+
+
+public class Robot 
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3];
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+        command?.Run(this);
+        WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = false;
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = true;
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X--; }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.X++; }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y--; }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot) { if (robot.IsPowered) robot.Y++; }
+}
